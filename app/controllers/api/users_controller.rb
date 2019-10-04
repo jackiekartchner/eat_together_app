2d6b707328f7900636042b79f5eb4cc84bcbecdc
jackie <jacqueline.kartchner@gmail.com>
2019-10-04 11:37:36 -0700

    adds restful routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,12 @@
 class Api::UsersController < ApplicationController
+
+#will not be needing a backend index, but wanted it here just for reference
+  def index
+    @users = User.all
+    render 'index.json.jb'
+  end
+
+  def show
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,23 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/users" => "users#index"
+    get "/users/:id" => "users#show"
+    post "/users" => "users#create"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
+
+    get "/bookings" => "bookings#index"
+    get "/bookings/:id" => "bookings#show"
+    post "/bookings" => "bookings#create"
+    patch "/bookings/:id" => "bookings#update"
+    delete "/bookings/:id" => "bookings#destroy"
+
+    get "/cravings" => "cravings#index"
+    get "/cravings/:id" => "cravings#show"
+    post "/cravings" => "cravings#create"
+    patch "/cravings/:id" => "cravings#update"
+    delete "/cravings/:id" => "cravings#destroy"
+  end
 end
